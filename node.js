2430ba9dd749e3e530ab568632c35783a77e75d9
James Sparling <jamesdsparling@gmail.com>
2022-03-07 14:29:54 +0000

    Create node.js
    Basic postgresql integration. Can connect to server and execute query

---
+++ node.js
@@ -0,0 +1,24 @@
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+    user: 'DefaultUser',
+    host: 'expertsoftware.duckdns.org',
+    database: 'SoftEng',
+    password: 'SecurePass',
+    port: 5432,
+})
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+const client = new Client({
+    user: 'DefaultUser',
+    host: 'expertsoftware.duckdns.org',
+    database: 'SoftEng',
+    password: 'SecurePass',
+    port: 5432,
+})
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+})
