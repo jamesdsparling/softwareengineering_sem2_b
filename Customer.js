170360424875f24b4e3bb7f7c525fac694157f1a
James Sparling <jamesdsparling@gmail.com>
2022-03-14 15:00:49 +0000

    Create Customer.js

---
+++ Customer.js
@@ -0,0 +1,24 @@
+class Customer {#
+    car;#
+    balance;
+    constructor(car, balance) {
+        this.car = car;
+        this.balance = balance;
+    }
+
+    getCar() {
+        return this.car;
+    }
+
+    getBalance() {
+        return this.balance;
+    }
+
+    setCar(car) {
+        this.car = car;
+    }
+
+    setBalance(balance) {
+        this.balance = balance;
+    }
+}
