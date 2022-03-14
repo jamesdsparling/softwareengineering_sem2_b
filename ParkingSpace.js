f03ff0dba93dd5963437ff54e0f7bf98987797f5
James Sparling <jamesdsparling@gmail.com>
2022-03-14 15:57:32 +0000

    Create ParkingSpace.js

---
+++ ParkingSpace.js
@@ -0,0 +1,11 @@
+class ParkingSpace {
+    space_ID;
+    isCharge;
+    location;
+
+    constructor(space_ID, isCharge, location) {
+        this.space_ID = space_ID;
+        this.isCharge = isCharge;
+        this.location = location;
+    }
+}
