01aaad7fe142074e8eff0fb46f3a848a75d9e265
James Sparling <jamesdsparling@gmail.com>
2022-03-14 14:35:43 +0000

    Formatting revisions

--- node.js
+++ node.js
@@ -1,23 +1,28 @@
 const { Pool, Client } = require('pg')
+
 const pool = new Pool({
     user: 'DefaultUser',
     host: 'expertsoftware.duckdns.org',
     database: 'SoftEng',
-    password: 'SecurePass',
+    password: '',
     port: 5432,
 })
+
 pool.query('SELECT NOW()', (err, res) => {
     console.log(err, res)
     pool.end()
 })
+
 const client = new Client({
     user: 'DefaultUser',
     host: 'expertsoftware.duckdns.org',
     database: 'SoftEng',
-    password: 'SecurePass',
+    password: '',
     port: 5432,
 })
+
 client.connect()
+
 client.query('SELECT NOW()', (err, res) => {
     console.log(err, res)
     client.end()
