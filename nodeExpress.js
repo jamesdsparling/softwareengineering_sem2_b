0b1d5389777d13ef947a7cbd797efc551b0d58f3
James Sparling <jamesdsparling@gmail.com>
2022-03-20 16:08:54 +0000

    Database password checking fully working. Admin authentication working.
    Passwords are stored and checked in plaintext still. Works for now but I will implement full hashing & encryption later.

--- nodeExpress.js
+++ nodeExpress.js
@@ -1,55 +1,89 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
+const express = require("express");
+const session = require("express-session");
+const fs = require("fs");
+const path = require("path");
+const { Client } = require("pg");
 // const { emitWarning } = require('process');
 
-const app = express()
+const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({
-    extended: true
-}))
+const buffer = fs.readFileSync("pass.txt");
 
-// const { Client } = require('pg');
-app.use(express.static('public'))
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(express.static("public"));
 
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
+app.get("/", function(req, res) {
+    res.redirect("/main");
+});
 
-
-app.get('/', function(req, res) {
-    res.redirect('/main')
-})
-
-app.get('/main', function(req, res) {
+app.get("/main", function(req, res) {
     //Check if logged in
     if (req.session.loggedin) {
-        res.sendFile(path.join(__dirname, '/private/main.html'))
+        if (req.session.admin) {
+            res.sendFile(path.join(__dirname, "/private/adminDashboard.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "/private/main.html"));
+        }
     } else {
-        res.redirect('signin.html')
+        res.redirect("signin.html");
     }
-})
+});
 
-
-app.post('/api/auth/signin', function(req, res) {
+app.post("/api/auth/signin", function(req, res) {
     // Ensure input fields not empty
     if (req.body.email && req.body.password) {
-        console.log("Sigining in: " + req.session.email);
-        req.session.loggedin = true;
-        req.session.email = req.body.email;
-        res.redirect('/main')
+        const client = new Client({
+            user: "DefaultUser",
+            host: "expertsoftware.duckdns.org",
+            database: "SoftEng",
+            password: buffer.toString(),
+            port: 5432,
+        });
+
+        client.connect();
+        client.query("SELECT profile_id, pass FROM profile WHERE email = $1", [req.body.email], (err, dbRes) => {
+            if (err) {
+                console.log(err.stack)
+            } else {
+                if (req.body.password == dbRes.rows[0].pass) {
+                    console.log("Sigining in: " + req.body.email);
+                    req.session.loggedin = true;
+                    req.session.email = req.body.email;
+                    if (dbRes.rows[0].profile_id == 'pf_0') {
+                        console.log("Signed in as admin user")
+                        req.session.admin = true;
+                    } else {
+                        req.session.admin = false;
+                    }
+                    res.redirect("/main");
+                } else {
+                    res.send("Incorrect password")
+                }
+            }
+            client.end()
+        });
+
     }
-})
+});
 
-app.post('/api/auth/signout', function(req, res) {
-    console.log("Signing out: " + req.session.email)
-    res.redirect('/main')
-    req.session.destroy()
-})
+app.post("/api/auth/signout", function(req, res) {
+    console.log("Signing out: " + req.session.email);
+    res.redirect("/main");
+    req.session.destroy();
+});
 
-app.listen(port, () => console.log('listening'));
+app.listen(port, () => console.log("listening"));
