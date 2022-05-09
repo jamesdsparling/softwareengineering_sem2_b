21d08d0af2e2e5e64e4514dc05ba1b623e400208
James Sparling <jamesdsparling@gmail.com>
2022-05-09 10:11:22 +0100

    Buttons to accept/decline ticket requests for admin

--- nodeExpress.js
+++ nodeExpress.js
@@ -41,7 +41,7 @@ function presentWithAccess(client, public, user, admin) {
             // Could possibly direct to index page but I like this implementation as users can bookmark the dashboard for easy access
             res.redirect(public);
         }
-    })
+    });
 }
 
 // Parse urlencoded payloads
@@ -67,59 +67,101 @@ app.use(
 
 // TEMPORARY CODE!!!!
 // Probably replace with an index type page once it has been made
-// Displayed page MUST offer a link to sign in/up 
+// Displayed page MUST offer a link to sign in/up
 app.get("/", function(req, res) {
     res.redirect("/Homepage.html"); // redirect("/index") ??? or maybe always attempt dashboard and redirect to index if not logged in
 });
 
-// Handle root address. Page served varies depending 
-presentWithAccess("/dashboard", "signin.html", "/UserPages/UserView.html", "/AdminPages/AdminViewMain.html")
-presentWithAccess("/tickets", "signin.html", "/UserPages/ManageTicket.html", "/AdminPages/AdminTicketReq.html")
-presentWithAccess("/help", "signin.html", "/UserPages/UserGetHelp.html", "/AdminPages/AdminViewMain.html")
-presentWithAccess("/addbalance", "signin.html", "/UserPages/AddBalance.html", "/AdminPages/AdminViewMain.html")
-presentWithAccess("/settings", "signin.html", "/UserPages/UserSettings.html", "/AdminPages/AdminViewMain.html")
-presentWithAccess("/messages", "signin.html", "/UserPages/UserMessages.html", "/AdminPages/AdminMessages.html")
-presentWithAccess("/map", "signin.html", "/UserPages/UserView.html", "/AdminPages/ManageMap.html")
-presentWithAccess("/modifyuser", "signin.html", "/UserPages/UserView.html", "/AdminPages/ModifyUser.html")
-
+// Handle root address. Page served varies depending
+presentWithAccess(
+    "/dashboard",
+    "signin.html",
+    "/UserPages/UserView.html",
+    "/AdminPages/AdminViewMain.html"
+);
+presentWithAccess(
+    "/tickets",
+    "signin.html",
+    "/UserPages/ManageTicket.html",
+    "/AdminPages/AdminTicketReq.html"
+);
+presentWithAccess(
+    "/help",
+    "signin.html",
+    "/UserPages/UserGetHelp.html",
+    "/AdminPages/AdminViewMain.html"
+);
+presentWithAccess(
+    "/addbalance",
+    "signin.html",
+    "/UserPages/AddBalance.html",
+    "/AdminPages/AdminViewMain.html"
+);
+presentWithAccess(
+    "/settings",
+    "signin.html",
+    "/UserPages/UserSettings.html",
+    "/AdminPages/AdminViewMain.html"
+);
+presentWithAccess(
+    "/messages",
+    "signin.html",
+    "/UserPages/UserMessages.html",
+    "/AdminPages/AdminMessages.html"
+);
+presentWithAccess(
+    "/map",
+    "signin.html",
+    "/UserPages/UserView.html",
+    "/AdminPages/ManageMap.html"
+);
+presentWithAccess(
+    "/modifyuser",
+    "signin.html",
+    "/UserPages/UserView.html",
+    "/AdminPages/ModifyUser.html"
+);
 
 app.post("/api/auth/signin", function(req, res) {
     // Ensure input fields not empty
     if (req.body.email && req.body.password) {
         // Query id and password for user with given email
-        client.query("SELECT profile_id, pass FROM profile WHERE email = $1", [req.body.email], (err, dbRes) => {
-            if (err) {
-                // Branch most likely reached if database is offline or connection interrupted
-                // Possibly want a formatted error but for now a stack trace is more useful
-                console.log(err.stack)
-            } else {
-                // Really bad code here. Rushed. Very insecure. Will change later 
-                if (dbRes.rows[0] && req.body.password == dbRes.rows[0].pass) { // Plain text password checking :( TODO
-                    console.log("Sigining in: " + req.body.email);
-
-                    // express-session setup
-                    req.session.loggedin = true;
-                    req.session.email = req.body.email;
-                    req.session.profile_id = dbRes.rows[0].profile_id
-
-                    // Assuming admin has id 0. Works so long as only ever one admin.
-                    // Might be worth storing this info in the database so that the admin user can be changed easily. Will look into this.
-                    if (dbRes.rows[0].profile_id == 1) {
-                        console.log("Signed in as admin user")
-                        req.session.admin = true;
-                    } else {
-                        req.session.admin = false;
-                    }
-
-                    res.redirect("/dashboard");
+        client.query(
+            "SELECT profile_id, pass FROM profile WHERE email = $1", [req.body.email],
+            (err, dbRes) => {
+                if (err) {
+                    // Branch most likely reached if database is offline or connection interrupted
+                    // Possibly want a formatted error but for now a stack trace is more useful
+                    console.log(err.stack);
                 } else {
-                    // Causes a redirect for now. Form resubmission is an issue and not a good UX
-                    // Will look in to some kind of jquery implementation instead
-                    res.send("Incorrect email or password")
+                    // Really bad code here. Rushed. Very insecure. Will change later
+                    if (dbRes.rows[0] && req.body.password == dbRes.rows[0].pass) {
+                        // Plain text password checking :( TODO
+                        console.log("Sigining in: " + req.body.email);
+
+                        // express-session setup
+                        req.session.loggedin = true;
+                        req.session.email = req.body.email;
+                        req.session.profile_id = dbRes.rows[0].profile_id;
+
+                        // Assuming admin has id 0. Works so long as only ever one admin.
+                        // Might be worth storing this info in the database so that the admin user can be changed easily. Will look into this.
+                        if (dbRes.rows[0].profile_id == 1) {
+                            console.log("Signed in as admin user");
+                            req.session.admin = true;
+                        } else {
+                            req.session.admin = false;
+                        }
+
+                        res.redirect("/dashboard");
+                    } else {
+                        // Causes a redirect for now. Form resubmission is an issue and not a good UX
+                        // Will look in to some kind of jquery implementation instead
+                        res.send("Incorrect email or password");
+                    }
                 }
             }
-        });
-
+        );
     }
 });
 
@@ -127,7 +169,7 @@ app.post("/api/auth/signout", function(req, res) {
     console.log("Signing out: " + req.session.email);
     // Destroy login session.
     req.session.destroy();
-    // See above comment about dashboard redirect. 
+    // See above comment about dashboard redirect.
     // DO NOT CHANGE THIS PLEASE, CHANGE THE GET LISTENER INSTEAD
     res.redirect("/");
 });
@@ -137,151 +179,214 @@ app.post("/api/auth/signup", function(req, res) {
     // Don't really care about any of the other data yet.
     // firstname / lastname / phonenumber also available in req.body
     if (req.body.email && req.body.password) {
-        client.query("INSERT INTO profile(email, pass) VALUES ($1, $2) RETURNING *", [req.body.email, req.body.password], (err, dbRes) => {
-            if (err) {
-                // Most common error. User already exists.
-                if (err.constraint == 'profile_email_key') {
-                    // Just redirects for now, will be fixed alongside sign in incorrect password resubmission
-                    res.send("User with email " + req.body.email + " already exists.")
+        client.query(
+            "INSERT INTO profile(email, pass) VALUES ($1, $2) RETURNING *", [req.body.email, req.body.password],
+            (err, dbRes) => {
+                if (err) {
+                    // Most common error. User already exists.
+                    if (err.constraint == "profile_email_key") {
+                        // Just redirects for now, will be fixed alongside sign in incorrect password resubmission
+                        res.send("User with email " + req.body.email + " already exists.");
+                    } else {
+                        // Other errors printed to console as a stack trace
+                        console.log(err.stack);
+                    }
                 } else {
-                    // Other errors printed to console as a stack trace
-                    console.log(err.stack);
-                }
-            } else {
-                console.log("New user created")
+                    console.log("New user created");
                     // Log the new user to the console
-                console.log(dbRes.rows[0])
+                    console.log(dbRes.rows[0]);
 
-                // Automatically logs in the user. No need to go back to signin.html anymore.
-                // req.session.loggedin = true;
-                // req.session.email = req.body.email;
+                    // Automatically logs in the user. No need to go back to signin.html anymore.
+                    // req.session.loggedin = true;
+                    // req.session.email = req.body.email;
 
-                // Attempt a dashboard redirect
-                // If login was unsuccessful this wil re-redirect to signin. (Possibly user has cookies disabled??)
-                res.redirect('/');
+                    // Attempt a dashboard redirect
+                    // If login was unsuccessful this wil re-redirect to signin. (Possibly user has cookies disabled??)
+                    res.redirect("/");
+                }
             }
-        })
+        );
     }
-})
+});
 
 app.post("/api/createTicket", function(req, res) {
     // Check if balance is enough
-    if (req.body.start_time, req.body.length, req.body.space_id) {
+    if ((req.body.start_time, req.body.length, req.body.space_id)) {
         // Maybe subtract in same query??o
-        let end_time = start_time + length
-        console.log(length)
-        client.query("INSERT INTO tickets(start_time, end_time, space_id) VALUES ($1, $2, $3) RETURNING *", [req.body.start_time, end_time, req.body.space_id], (err, dbRes) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                // Subtract from balance
-                console.log("New ticket created")
-                console.log(dbRes.rows[0])
-                res.redirect("/dashboard")
+        let end_time = start_time + length;
+        console.log(length);
+        client.query(
+            "INSERT INTO tickets(start_time, end_time, space_id) VALUES ($1, $2, $3) RETURNING *", [req.body.start_time, end_time, req.body.space_id],
+            (err, dbRes) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    // Subtract from balance
+                    console.log("New ticket created");
+                    console.log(dbRes.rows[0]);
+                    res.redirect("/dashboard");
+                }
             }
-        })
+        );
     }
-})
+});
 
 app.post("/api/me/updatePlate", function(req, res) {
     if (req.body.registration_plate) {
-        client.query("UPDATE profile SET registration_plate = $1 WHERE profile_id = $2 RETURNING *", [req.body.registration_plate, req.session.profile_id], (err, dbRes) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                console.log("Registration plate updated")
-                console.log(dbRes.rows[0])
-                res.redirect('back')
+        client.query(
+            "UPDATE profile SET registration_plate = $1 WHERE profile_id = $2 RETURNING *", [req.body.registration_plate, req.session.profile_id],
+            (err, dbRes) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    console.log("Registration plate updated");
+                    console.log(dbRes.rows[0]);
+                    res.redirect("back");
+                }
             }
-        })
+        );
     }
-})
+});
 
 app.post("/api/admin/updateTicketStatus", function(req, res) {
-    if (req.body.ticket_id, req.body.status) {
-        client.query("UPDATE tickets SET status = $1 WHERE ticket_id = $2 RETURNING *", [req.body.status, req.body.ticket_id], (err, dbRes) => {
-            if (err) {
-                console.log(err.stack)
+    if (req.session.admin) {
+        if ((req.body.ticket_id, req.body.is_accepted)) {
+            if (req.body.is_accepted == true) {
+                client.query(
+                    "UPDATE ticket SET is_accepted = true WHERE ticket_id = $1 RETURNING *", [req.body.ticket_id],
+                    (err, dbRes) => {
+                        if (err) {
+                            console.log(err.stack);
+                        } else {
+                            console.log("Ticket status updated");
+                            console.log(dbRes.rows[0]);
+                            res.redirect("/dashboard");
+                        }
+                    }
+                );
             } else {
-                console.log("Ticket status updated")
-                console.log(dbRes.rows[0])
-                res.redirect("/dashboard")
+                client.query(
+                    "DELETE FROM ticket WHERE ticket_id = $1 RETURNING *", [req.body.ticket_id],
+                    (err, dbRes) => {
+                        if (err) {
+                            console.log(err.stack);
+                        } else {
+                            console.log("Ticket status updated");
+                            console.log(dbRes.rows[0]);
+                            res.redirect("/dashboard");
+                        }
+                    }
+                );
             }
-        })
+        }
     }
-})
+});
 
 app.post("/api/me/tickets", function(req, res) {
-    client.query("SELECT ticket_id, start_time, end_time FROM ticket WHERE ticket.profile_id = $1", [req.session.profile_id], (err, dbRes) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            res.send(dbRes.rows)
+    client.query(
+        "SELECT ticket_id, start_time, end_time FROM ticket WHERE ticket.profile_id = $1", [req.session.profile_id],
+        (err, dbRes) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                res.send(dbRes.rows);
+            }
         }
-    })
-})
+    );
+});
+
+app.post("/api/admin/tickets", function(req, res) {
+    if (req.session.admin) {
+        client.query(
+            "SELECT ticket_id, start_time, end_time FROM ticket",
+            (err, dbRes) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    res.send(dbRes.rows);
+                }
+            }
+        );
+    }
+});
 
 app.post("/api/me/messages", function(req, res) {
-    client.query("SELECT * FROM messages WHERE from_profile = $1 OR to_profile = $1", [req.session.profile_id], (err, dbRes) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            res.send(dbRes.rows)
+    client.query(
+        "SELECT * FROM messages WHERE from_profile = $1 OR to_profile = $1", [req.session.profile_id],
+        (err, dbRes) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                res.send(dbRes.rows);
+            }
         }
-    })
-})
+    );
+});
 
 app.post("/api/sendMessage", function(req, res) {
     if (req.body.message) {
-        client.query("INSERT INTO messages(message, from_profile, to_profile) VALUES ($1, $2, $3) RETURNING *", [req.body.message, req.session.profile_id, 1], (err, dbRes) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                console.log("Sent message")
-                console.log(dbRes.rows[0])
-                res.redirect("/dashboard")
+        client.query(
+            "INSERT INTO messages(message, from_profile, to_profile) VALUES ($1, $2, $3) RETURNING *", [req.body.message, req.session.profile_id, 1],
+            (err, dbRes) => {
+                if (err) {
+                    console.log(err.stack);
+                } else {
+                    console.log("Sent message");
+                    console.log(dbRes.rows[0]);
+                    res.redirect("/dashboard");
+                }
             }
-        })
+        );
     }
-})
+});
 
 app.post("/api/admin/sendMessage", function(req, res) {
-    if (req.body.message, to_profile) {
-        client.query("INSERT INTO messages(message, from_profile, to_profile) VALUES ($1, $2, $3) RETURNING *", [req.body.message, 1, req.body.to_profile], (err, dbRes) => {
-            if (err) {
-                console.log(err.stack)
-            } else {
-                console.log("Sent message")
-                console.log(dbRes.rows[0])
-                res.redirect("/dashboard")
-            }
-        })
+    if (req.session.admin) {
+        if ((req.body.message, to_profile)) {
+            client.query(
+                "INSERT INTO messages(message, from_profile, to_profile) VALUES ($1, $2, $3) RETURNING *", [req.body.message, 1, req.body.to_profile],
+                (err, dbRes) => {
+                    if (err) {
+                        console.log(err.stack);
+                    } else {
+                        console.log("Sent message");
+                        console.log(dbRes.rows[0]);
+                        res.redirect("/dashboard");
+                    }
+                }
+            );
+        }
     }
-})
+});
 
 app.post("/api/admin/updateProfile", function(req, res) {
-    if (req.body.profile_id) {
-        if (req.body.email) {
-            updateProfile("email", req.body.email, req.body.profile_id)
-        }
-        if (req.body.pass) {
-            updateProfile("pass", req.body.pass, req.body.profile_id)
+    if (req.session.admin) {
+        if (req.body.profile_id) {
+            if (req.body.email) {
+                updateProfile("email", req.body.email, req.body.profile_id);
+            }
+            if (req.body.pass) {
+                updateProfile("pass", req.body.pass, req.body.profile_id);
+            }
         }
     }
-})
+});
 
 // app.post("/api/getSpaces")
 
 function updateProfile(profile_id, field, value) {
-    client.query("UPDATE profile SET $1 = $2 WHERE profile_id = $2 RETURNING *", [field, value, profile_id], (err, dbRes) => {
-        if (err) {
-            console.log(err.stack)
-        } else {
-            console.log("Profile updated")
-            console.log(dbRes.rows[0])
-            res.redirect("/dashboard")
+    client.query(
+        "UPDATE profile SET $1 = $2 WHERE profile_id = $2 RETURNING *", [field, value, profile_id],
+        (err, dbRes) => {
+            if (err) {
+                console.log(err.stack);
+            } else {
+                console.log("Profile updated");
+                console.log(dbRes.rows[0]);
+                res.redirect("/dashboard");
+            }
         }
-    })
+    );
 }
 
 app.listen(port, () => console.log("listening"));
