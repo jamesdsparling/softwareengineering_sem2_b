85b20d153335f4625bfd18e625115136ba58c43e
James Sparling <jamesdsparling@gmail.com>
2022-05-09 14:21:03 +0100

    Messaging api backend improvements

--- nodeExpress.js
+++ nodeExpress.js
@@ -303,6 +303,7 @@ app.post("/api/admin/tickets", function(req, res) {
                 if (err) {
                     console.log(err.stack);
                 } else {
+                    console.log(dbRes.rows)
                     res.send(dbRes.rows);
                 }
             }
